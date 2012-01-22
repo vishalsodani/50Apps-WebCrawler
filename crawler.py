45addf579a6e2b02e94d393aa4ddec4dc06ddb85
unknown <admin@.(none)>
2012-01-22 10:16:02 +0530

    first success with crawler using "SOPA" as searchtext and http://news.ycombinator.com/ as url

--- crawler.py
+++ crawler.py
@@ -1,16 +1,31 @@
 from BeautifulSoup import BeautifulSoup
 import sys
 import requests
+import traceback
+import re
 
-def crawl_url(url):
+def crawl_url(url, searchtext, depth = 1):
     r = requests.get(url)
+    urls_with_search_text = []
     soup = BeautifulSoup(r.content)
-    links = soup.findAll('a')
+    links = soup.findAll(href=re.compile('^http'))
     for link in links:
-        print link['href']
+        try:
+            r = requests.get(link['href'])
+            soup = BeautifulSoup(r.content)
+            found = soup.findAll(text=re.compile('^SOPA'))
+            if len(found) > 0:
+                urls_with_search_text.append(link['href'])
+        except:
+            traceback.print_exc()
+            print link
+
+    print urls_with_search_text
+        
     
 
 if __name__ == '__main__':
     var = raw_input("Enter url to crawl: ")
-    crawl_url(var)
+    searchtext = raw_input("Enter seacrhtext: ")
+    crawl_url(var,searchtext)
 
