4871fdabda4548c48e3d75eb5e28eaea059bafff
unknown <admin@.(none)>
2012-01-22 10:18:43 +0530

    able to get all links

--- crawler.py
+++ crawler.py
@@ -1,8 +1,14 @@
 from BeautifulSoup import BeautifulSoup
 import sys
+import requests
 
 def crawl_url(url):
-    print url
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content)
+    links = soup.findAll('a')
+    for link in links:
+        print link['href']
+    
 
 if __name__ == '__main__':
     var = raw_input("Enter url to crawl: ")
