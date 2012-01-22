840f8e51a1e246237f43966b06952048acb50494
unknown <admin@.(none)>
2012-01-22 10:35:25 +0530

    removed counter used for testing, works for depth 3, extremely slow

--- crawler.py
+++ crawler.py
@@ -3,8 +3,12 @@ import requests
 import traceback
 import re
 
-def crawl_url(url, searchtext):
-    web_page = requests.get(url)
+URL_TO_CRAWL = ""
+SEARCH_TEXT = ""
+
+
+def crawl_url():
+    web_page = requests.get(URL_TO_CRAWL)
     urls_with_search_text = []
     soup = BeautifulSoup(web_page.content)
     links = soup.findAll(href=re.compile('^http'))
@@ -17,15 +21,11 @@ def process_links(links, depth):
     urls_with_search_text = []
     if depth > 3:
         return urls_with_search_text
-    counter = 0
     for link in links:
-        if counter > 10:
-            break
-        counter = counter + 1    
         try:
             web_page = requests.get(link['href'])
             soup = BeautifulSoup(web_page.content)
-            found = soup.findAll(text=re.compile('^' + searchtext))
+            found = soup.findAll(text=re.compile('^' + SEARCH_TEXT))
             if len(found) > 0:
                 urls_with_search_text.append(link['href'])
             links_new = soup.findAll(href=re.compile('^http'))
@@ -40,7 +40,7 @@ def process_links(links, depth):
     
 
 if __name__ == '__main__':
-    url = raw_input("Enter url to crawl: ")
-    searchtext = raw_input("Enter seacrhtext: ")
-    crawl_url(url, searchtext)
+    URL_TO_CRAWL = raw_input("Enter url to crawl: ")
+    SEARCH_TEXT = raw_input("Enter searchtext: ")
+    crawl_url()
 
