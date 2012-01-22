c884213da2c187adc50bb333e24383cc5bcb7475
unknown <admin@.(none)>
2012-01-22 10:08:25 +0530

    just added main to take input and print

---
+++ crawler.py
@@ -0,0 +1,10 @@
+from BeautifulSoup import BeautifulSoup
+import sys
+
+def crawl_url(url):
+    print url
+
+if __name__ == '__main__':
+    var = raw_input("Enter url to crawl: ")
+    crawl_url(var)
+
